4d8efe5c353ba72086b221f3e6dfcd634dbf83a1
gabsuzzer <gabsuzzer@gmail.com>
2023-03-17 14:04:37 -0700

    Patience, Persistence

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+1 lines (20 sloc)  219 Bytes
+
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	return (0);
+}
