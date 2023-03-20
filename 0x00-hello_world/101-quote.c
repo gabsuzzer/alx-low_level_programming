a4f3efad0a6bc78c2a9fea7da618729d1b56d2c8
gabsuzzer <gabsuzzer@gmail.com>
2023-03-20 16:46:00 -0700

    Unix is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+13 lines (12 sloc)  198 Bytes
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
