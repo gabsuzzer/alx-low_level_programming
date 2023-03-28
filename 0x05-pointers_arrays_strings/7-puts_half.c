283e4cf98e9add010e177f0314fdf25561497b97
gabsuzzer <gabsuzzer@gmail.com>
2023-03-27 18:08:13 -0700

    Prints half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* puts_half - prints half of a string followed by new line
+* @str: string to print half of
+*
+* Return: nothing
+*/
+void puts_half(char *str)
+{
+	int i = 0, len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
