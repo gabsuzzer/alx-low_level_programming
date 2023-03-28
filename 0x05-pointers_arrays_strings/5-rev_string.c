3fdf251768e7317bb7eaaa15b48a5028f06e26c9
gabsuzzer <gabsuzzer@gmail.com>
2023-03-27 17:56:40 -0700

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* rev_string - reverses a string
+* @s: string being reversed
+*
+* Return: nothing
+*/
+void rev_string(char *s)
+{
+	int i = 0, c = 0, last;
+	char temp;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		s--;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
+		last = c - 1;
+		temp = s[last - i];
+		s[last - i] = s[i];
+		s[i] = temp;
+	}
+}
