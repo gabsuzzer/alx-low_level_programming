c2d5a04d0eee019d4e28ce1326742a7e0403836b
gabsuzzer <gabsuzzer@gmail.com>
2023-04-04 05:44:48 +0300

    a function to search a string in a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,14 @@
 #include "main.h"
 char *_strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-    while (*s)
-    {
-        if (strchr(accept, *s))
-            return s;
-        s++;
+    for (size_t i = 0; s[i] != '\0'; i++) {
+        for (size_t j = 0; accept[j] != '\0'; j++) {
+            if (s[i] == accept[j]) {
+                return &s[i];
+            }
+        }
     }
-    return NULL;
+
+    return (void *) 0;
 }
