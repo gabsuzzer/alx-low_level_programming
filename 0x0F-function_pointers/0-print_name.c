22b45ed475d4e493df98b3d4ee632877c3ee8374
gabsuzzer <gabsuzzer@gmail.com>
2023-04-21 12:17:08 +0300

    correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
-
+/**
+* print_name - prints a name
+* @name: name to be printed
+* @f: pointer to funtion used for printing name
+*
+* Return: nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
     (*f)(name);
